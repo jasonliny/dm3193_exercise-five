19b387876b8792b8cfe6c186ce48b31a2e8c23bd
Jason <70929180+jasonliny@users.noreply.github.com>
2022-11-08 09:18:27 -0500

    Setup firebase

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const app = express();
-const router = express.Router();
+const firebase = require("firebase/app");
 const port = process.env.PORT || 4000;
 
-const homePage = require("./routes/index.js");
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDo3y6B_zMIl7U8SZelw73Q5nV2Ruyy7rg",
+
+  authDomain: "dm3193-exercise-five.firebaseapp.com",
+
+  projectId: "dm3193-exercise-five",
+
+  storageBucket: "dm3193-exercise-five.appspot.com",
+
+  messagingSenderId: "1085101582590",
+
+  appId: "1:1085101582590:web:c5166b0de95f882d355daf",
+};
+
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
+
+const homePage = require("./routes/index");
+const singlePostRouter = require("./routes/singlePost");
 
 app.use("/", homePage);
+app.use("/post/", singlePostRouter);
 
 app.listen(port, () => {
   console.log("Listening");
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const firestore = require("firebase/firestore");
+const db = firestore.getFirestore();
 
 router.use((req, res, next) => {
   console.log("Time: ", Date.now());
   next();
 });
 router.get("/", (req, res) => {
-  res.send("Hello");
+  const postsQuery = firestore.getDocs(firestore.collection(db, "posts"));
+  const postsArray = [];
+
+  postsQuery
+    .then((response) => {
+      response.forEach((post) => {
+        console.log(post.data());
+        postsArray.push({ id: post.id, ...post.data() });
+      });
+      res.send(postsArray);
+    })
+    .catch(function (error) {
+      console.log("error:", error);
+      return res.send(error);
+    });
 });
 
 module.exports = router;
